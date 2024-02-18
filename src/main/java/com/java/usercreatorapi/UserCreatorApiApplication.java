12d3a909f033c53b80323c6dc40e7ebff517af72
rsandovalb <31744584+rsandovalb@users.noreply.github.com>
2024-02-18 15:56:52 -0300

    First commit

---
+++ src/main/java/com/java/usercreatorapi/UserCreatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.usercreatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCreatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/usercreatorapi/UserCreatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.usercreatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
